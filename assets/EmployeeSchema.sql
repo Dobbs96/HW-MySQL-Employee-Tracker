241c09c4ed2282474ae39c60ec966adb65b06241
James Dobbs <dobbs282@gmail.com>
2021-08-14 12:07:37 -0400

    added Data Base

--- assets/EmployeeSchema.sql
+++ assets/EmployeeSchema.sql
@@ -7,23 +7,47 @@ USE Emplouyee_DB;
 -- Data Tables --
 
 -- departments --
+CREATE TABLE departments (
 ---- id INT AUTO_INCREMENT
+id INT NOT NULL AUTO_INCREMENT,
 ---- name VARCHAR(30) no null
+name VARCHAR(30) NOT NULL,
 ---- PRIMARY KEY(id)
+PRIMARY KEY (id)
+);
 
 -- roles --
+CREATE TABLE roles (
 ---- id INT no null AUTO_INCREMENT
+id INT NOT NULL AUTO_INCREMENT,
 ---- title no null
+title VARCHAR(30) NOT NULL,
 ---- salary no null  --DECIMAL to hold role salary
+salary DECIMAL(10,2) NOT NULL,
 ---- department_id INT
+department_id INT,
 ---- PRIMARY KEY(id)
----- FOREIGN KEY (department_id) REFERENCES department(id)
+PRIMARY KEY (id),
+---- FOREIGN KEY (department_id) REFERENCES departments(id)
+FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
 
 -- employees --
+CREATE TABLE employees (
 ---- id INT no null AUTO_INCREMENT
+id INT NOT NULL AUTO_INCREMENT,
 ---- first_name VARCHAR(30) no null
----- last_name VARCHAR(30) no null  --DECIMAL to hold role salary
+first_name VARCHAR(30) NOT NULL,
+---- last_name VARCHAR(30) no null 
+last_name VARCHAR(30) NOT NULL,
 ---- role_id INT no null
+role_id INT NOT NULL,
 ---- manager_id INT can be null   --- link name of manager
+manager_id INT,
 ---- PRIMARY KEY(id)
----- FOREIGN KEY (role_id) REFERENCES department(id)
+PRIMARY KEY (id),
+---- FOREIGN KEY (role_id) REFERENCES departments(id)
+FOREIGN KEY (role_id) REFERENCES roles(id),
+FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
--- assets/EmployeeSeeds.sql
+++ assets/EmployeeSeeds.sql
@@ -2,18 +2,36 @@
 -- Data added first 5 Employees -- 
 
 -- departments ---- (name)
+INSERT INTO departments (name)
+VALUES  ("Accounting"), ("Sales"), ("Engineering"), ("Legal"), ("Office");
 
-
--- roles ---- (name, title, salary, department_id)
-
+-- roles ---- (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Accountant Lead", 135000, 1), ("Accountant", 112000, 1), ("Accountant Intern", 45000, 1), ("Sales Lead", 102000, 2), ("Sales Person", 90000, 2), ("Sales Intern",43000, 2), ("Engineer Lead", 132000, 3), ("Engineer", 113000, 3), ("Engineer Intern", 57000, 3), ("Lawyer Lead", 142000, 4), ("Lawyer", 128000, 4), ("Office Manager", 98000, 5), ("Office Assistant", 87000, 5), ("Office Intern", 32000, 5);
 
 -- employees ---- (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("James", "Dobbs", 7);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 3, 1);
 
 
--- SELECTS --
+--              SELECTS --
+--ALL--
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
 
--- UPDATES --
+-- View All Employees CONCAT -- 
 
--- DELETE --
+-- View All Employees by Department -- 
 
--- ADD --
+-- View All Employees by Manager --
+
+-- View All Employees by Title -- 
+
+--              UPDATES --
+
+--              DELETE --
+
+--              ADD --
